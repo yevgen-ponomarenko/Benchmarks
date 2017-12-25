2a27d7ba2453d4aaa412f62a74c7fa779da69f4a
yevgen-ponomarenko <yevgen.ponomarenko@gmail.com>
2017-12-25 22:52:21 +0200

    Syncfusion benchmark kick-off

---
+++ NetStandard.PDF/NetStandard.PDF/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace NetStandard.PDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Syncfusion>();
+        }
+
+        public class Syncfusion
+        {
+            public Syncfusion()
+            {
+            }
+
+            [Benchmark]
+            public void Split_50Mb_1000pages_by_2_pages()
+            {
+            }
+
+            [Benchmark]
+            public void Split_50Mb_1000pages_by_10_pages()
+            {
+            }
+        }
+    }
+}
